0561ecc65bace604f7b3f9725c20f9ae9fb67657
REM <fh831.cp9gw@gmail.com>
2025-08-09 22:18:01 +0800

    fix: 提交前端文件

--- frontend/src/components/layout/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <n-space justify="center">
-    <span>&copy; 2024 {{ $t('nav.home') }} - {{ $t('common.info') }}</span>
-  </n-space>
-</template>
-
-<script setup lang="ts">
-// Footer component
-</script>
--- frontend/src/composables/useI18n.ts
+++
@@ -1,5 +0,0 @@
-import { useI18n as useVueI18n } from 'vue-i18n'
-
-export function useI18n() {
-  return useVueI18n()
-}
